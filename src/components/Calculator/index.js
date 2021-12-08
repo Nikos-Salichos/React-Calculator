912bc409d7ac83bbd4b81652f87fc8bd8e5bca75
Nikos S <niksal1991@gmail.com>
2021-12-08 19:53:52 +0200

    Add button changes

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Screen, Previous, Current, Button } from './Styled';
+
+export default function Calculator() {
+    return (
+        <Container>
+            <Screen>
+                <Previous>10+</Previous>
+                <Current>100</Current>
+            </Screen>
+            <Button gridSpan={2}>AC</Button>
+            <Button control>DEL</Button>
+            <Button operation>+</Button>
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button operation>*</Button>
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button operation>-</Button>
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button operation>/</Button>
+            <Button period>.</Button>
+            <Button>0</Button>
+            <Button gridSpan={2}>=</Button>
+        </Container>
+    )
+}
