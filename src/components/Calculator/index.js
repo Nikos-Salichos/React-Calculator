4c1b4d031daaddfea462ba59aa15611697986ff4
Nikos S <niksal1991@gmail.com>
2021-12-10 19:34:52 +0200

    final version

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -3,7 +3,9 @@ import { Container, Screen, Previous, Current, Button } from './Styled';
 
 export default function Calculator() {
 
+    const [previous, setPrevious] = useState('')
     const [current, setCurrent] = useState('')
+    const [operation, setOperation] = useState('')
 
     const appendValue = (el) => {
         const value = el.target.getAttribute('data');
@@ -14,30 +16,92 @@ export default function Calculator() {
         setCurrent(current + value);
     }
 
+    const handleDelete = () => {
+        let newCurrent = current.slice(0, -1);
+        setCurrent(String(newCurrent));
+    }
+
+    const handleAllClear = () => {
+        setCurrent('')
+        setPrevious('')
+        setOperation('')
+    }
+
+    const chooseOperation = (el) => {
+        if (current === '') {
+            return;
+        }
+
+        if (previous !== '') {
+            let value = compute();
+            setPrevious(value);
+        } else {
+            setPrevious(current)
+        }
+
+        setCurrent('');
+        setOperation(el.target.getAttribute('data'))
+    }
+
+    const equals = () => {
+        let value = compute();
+        if (value === undefined || value == null) {
+            return;
+        }
+
+        setCurrent(value)
+        setPrevious('')
+        setOperation('')
+    }
+
+    const compute = () => {
+        let result;
+        let previousNumber = parseFloat(previous);
+        let currentNumber = parseFloat(current);
+
+        if (isNaN(previousNumber) || isNaN(currentNumber)) {
+            return;
+        }
+
+        if (operation === '+') {
+            result = previousNumber + currentNumber;
+        } else if (operation === '-') {
+            result = previousNumber - currentNumber;
+        } else if (operation === '*') {
+            result = previousNumber * currentNumber;
+        } else if (operation === '/') {
+            result = previousNumber / currentNumber;
+        } else {
+            result = ('');
+        }
+
+        return result;
+    }
+
     return (
         <Container>
             <Screen>
-                <Previous>10 +</Previous>
+                <Previous>{previous} {operation}</Previous>
                 <Current>{current}</Current>
             </Screen>
-            <Button gridSpan={2} control>AC</Button>
-            <Button control>DEL</Button>
-            <Button operation>+</Button>
+            <Button onClick={handleAllClear} gridSpan={2} control>AC</Button>
+            <Button onClick={handleDelete} control>DEL</Button>
+            <Button data={'+'} onClick={chooseOperation} operation>+</Button>
             <Button data={'7'} onClick={appendValue}>7</Button>
             <Button data={'8'} onClick={appendValue}>8</Button>
             <Button data={'9'} onClick={appendValue}>9</Button>
-            <Button data={'*'} period onClick={appendValue} operation>*</Button>
+            <Button data={'*'} onClick={chooseOperation} operation>*</Button>
             <Button data={'4'} onClick={appendValue}>4</Button>
             <Button data={'5'} onClick={appendValue}>5</Button>
             <Button data={'6'} onClick={appendValue}>6</Button>
-            <Button data={'-'} period onClick={appendValue} operation>-</Button>
+            <Button data={'-'} onClick={chooseOperation} operation>-</Button>
             <Button data={'1'} onClick={appendValue}>1</Button>
             <Button data={'2'} onClick={appendValue}>2</Button>
             <Button data={'3'} onClick={appendValue}>3</Button>
-            <Button data={'/'} period onClick={appendValue} operation >/</Button>
+            <Button data={'/'} onClick={chooseOperation} operation >/</Button>
             <Button data={'.'} period onClick={appendValue}>.</Button>
             <Button data={'0'} onClick={appendValue}>0</Button>
-            <Button gridSpan={2}>=</Button>
+            <Button data gridSpan={2} onClick={equals} equals>=</Button>
         </Container>
     )
 }
